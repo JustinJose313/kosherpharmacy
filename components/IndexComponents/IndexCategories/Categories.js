371e081a6273f84a121c8731daef119b07572a2e
JUSTIN <justinjose320@gmail.com>
2021-06-13 16:17:52 +0530

    part1 json added

--- components/IndexComponents/IndexCategories/Categories.js
+++ components/IndexComponents/IndexCategories/Categories.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import { productData, productHeadings } from "../../../public/data/productData";
 
 const Categories = () => {
   const data = [...Array(5)];
-  const catData = [...Array(11)];
   const [toggle, setToggle] = useState(true);
+  const [category, setCategory] = useState("Antacids");
+
   return (
-    <div id='categories'>
+    <div id="categories">
       <div className="max-w-7xl mx-auto px-4 md:px-8 py-8">
         <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
           <div className="md:col-span-4 mb-6">
@@ -22,14 +24,19 @@ const Categories = () => {
                 Show Categories
               </div>
               {toggle && (
-                <div className='border'>
-                  {catData.map((each, i) => {
+                <div className="border">
+                  {productHeadings.map((each, i) => {
                     return (
                       <div
+                        onClick={() => setCategory(each)}
                         key={i}
-                        className="bg-white cursor-pointer hover:bg-gray-50 font-semibold hover:text-brand-200 p-4 w-full"
+                        className={`${
+                          category === each
+                            ? "bg-gray-100 text-brand-200"
+                            : "bg-white"
+                        } cursor-pointer hover:bg-gray-50 font-semibold hover:text-brand-200 p-4 w-full`}
                       >
-                        Allergies
+                        {each}
                       </div>
                     );
                   })}
@@ -38,35 +45,52 @@ const Categories = () => {
             </div>
           </div>
           <div className="md:col-span-8 space-y-4">
-            {data.map((each, i) => {
-              return (
-                <div
-                  key={i}
-                  className="bg-whiite border-2 border-dashed flex flex-col md:flex-row w-full"
-                >
-                  <img
-                    className="w-full md:w-4/12"
-                    src="https://pharmacare.qodeinteractive.com/wp-content/uploads/2021/03/Product-featured-img-21.jpg"
-                    alt=""
-                  />
-                  <div className="flex-1 p-4">
-                    <h2 className="uppercase font-semibold">
-                      Vicky liftactive supreme serum
-                    </h2>
-                    <p className="text-gray-500">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Aliquid quam at natus saepe sequi id nobis mollitia quasi
-                      ducimus labore!
-                    </p>
-                    <p className="font-semibold text-xl mt-6 text-brand-200">
-                      Rs.499 /-
-                    </p>
-                    <button className="text-white shadow-md bg-brand-100 hover:bg-brand-200 px-5 py-2 mt-4 transition">
-                      Add to Cart
-                    </button>
+            {productData.map((each, i) => {
+              if (category === each.c) {
+                return (
+                  <div
+                    key={i}
+                    className="bg-whiite border-2 border-dashed flex flex-col md:flex-row md:items-start w-full"
+                  >
+                    <img
+                      className="w-full md:w-4/12"
+                      src="https://pharmacare.qodeinteractive.com/wp-content/uploads/2021/03/Product-featured-img-21.jpg"
+                      alt=""
+                    />
+                    <div className="flex-1 p-4">
+                      <h2 className="uppercase font-semibold">{each.n}</h2>
+                      {each.d.map((e, i) => {
+                        return (
+                          <p className="text-gray-500 my-2" key={i}>
+                            {e}
+                          </p>
+                        );
+                      })}
+                      {each.ul && (
+                        <div>
+                          <p className="text-gray-500 mt-4 mb-2">{each.ul.h}</p>
+
+                          <ul className='ml-6'>
+                            {each.ul.list.map((l, i) => {
+                              return (
+                                <li key={i}>
+                                  <p className='text-gray-500 capitalize'>{l}</p>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </div>
+                      )}
+                      <p className="font-semibold text-xl mt-6 text-brand-200">
+                        Rs.499 /-
+                      </p>
+                      <button className="text-white shadow-md bg-brand-100 hover:bg-brand-200 px-5 py-2 mt-4 transition">
+                        Add to Cart
+                      </button>
+                    </div>
                   </div>
-                </div>
-              );
+                );
+              }
             })}
           </div>
         </div>
---
+++ public/data/productData.js
@@ -0,0 +1,264 @@
+export const productData = [
+  {
+    c: "Antacids",
+    n: "Lansoprazole Capsules",
+    id: "CAS NO.: 103577-45-3",
+    d: [
+      "Lansoprazole is used in treatment of  ulcers of the stomach and duodenum, NSAID-induced ulcers and in the treatment of gastroesophageal reflux disease.",
+    ],
+  },
+  {
+    c: "Antacids",
+    n: "Omeprazole Capsules",
+    id: "CAS NO:73590-58-6",
+    d: [
+      "Omeprazole belongs to a group of drugs called proton pump inhibitors. It decreases the amount of acid produced in the stomach.",
+      "Omeprazole is used to treat symptoms of gastroesophageal reflux disease (GERD) and other conditions caused by excess stomach acid. It is also used to promote healing of erosive esophagitis (damage to your esophagus caused by stomach acid).",
+      "It may also be given together with antibiotics to treat gastric ulcer caused by infection with helicobacter pylori (H. pylori).",
+      "Omeprazole is available in 10 mg, 20 mg, 40 mg",
+    ],
+  },
+  {
+    c: "Antacids",
+    n: "Pantoprazole Tablets & Injections",
+    id: "CAS NO:164579-32-2",
+    d: [
+      "Pantoprazole used for short-term treatment of erosion and ulceration of the esophagus caused by gastro esophageal reflux disease.",
+      "Pantoprazole, like other proton-pump inhibitors, blocks the enzyme in the wall of the stomach that produces acid. By blocking the enzyme, the production of acid is decreased, and this allows the stomach and esophagus to heal.",
+      "Pantoprazole is available in 20mg & 40mg tablets. It is also available in injections.",
+    ],
+  },
+  {
+    c: "Antacids",
+    n: "Ranitidine Tablets and Injection",
+    id: "CAS NO: 66357-35-5",
+    d: [
+      "Ranitidine is used to treat ulcers; gastro esophageal reflux disease (GERD), a condition in which backward flow of acid from the stomach causes heartburn and injury of the food pipe (esophagus); and conditions where the stomach produces too much acid",
+      "Ranitidine is available in 150 mg, 300 mg tablets. It is also available in injection.",
+    ],
+  },
+  {
+    c: "Anthelmintics",
+    n: "Albendazole Tablets",
+    id: "CAS NO:54965-21-8",
+    d: [
+      "Albendazole 400 mg tablets are used for treating certain tapeworm infections. Albendazole is an anthelmintic. It works by killing sensitive parasites.",
+    ],
+  },
+  {
+    c: "Anthelmintics",
+    n: "Mebendazole Tablets",
+    id: "CAS NO: 31431-39-7",
+    d: [
+      "It is used to treat infestations by worms including Hook worms, Tapeworms Roundworms, Whipworms, Threadworms or Pinworms.",
+    ],
+  },
+  {
+    c: "Antibacterial Drugs",
+    n: "Tinidazole Tablets",
+    id: "CAS NO: 19387-91-8",
+    d: [
+      "Tinidazole is used to treat trichomoniasis (a sexually transmitted disease that can affect men and women), giardiasis (an infection of the intestine that can cause diarrhea, gas, and stomach cramps), and amebiasis (an infection of the intestine that can cause diarrhea, gas, and stomach cramps and can spread to other organs such as the liver). Tinidazole is in a class of medications called antiprotozoal agents. It works by killing the organisms that can cause infection.Tinidazole is taken with food as a single dose or once a day for 3 to 5 days.",
+    ],
+  },
+  {
+    c: "Antibacterial Drugs",
+    n: "Secnidazole Tablets",
+    id: "CAS NO.: 3366-95-8",
+    d: [
+      "Secnidazole  is a nitro-imidazole anti-infective and is effective in the treatment of dientamoebiais.",
+      "Adult: PO- Invasive amoebiasis- The recommended dose is 1.5 g/day for 5 days.",
+      "Amoebiasis; Giardiasis; Trichomoniasis Single dose of 2 gm.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Cefotaxime Sodium Injection ",
+    id: "CAS NO:64485-93-4.",
+    d: [
+      "Cefotaxime is an antibiotic used to treat a wide variety of bacterial infections. Cefotaxime Sodium for Injection is a broad spectrum bactericidal cephalosporin antibiotic. Cefotaxime Sodium Injection is available in compositions of 250mg, 500mg, 1 gm injectables.",
+      "It is used in treatment of infections of lower respiratory tract including pneumonia, urinary tract, skin and skin structures, bone and joints, bacteremia/septicemia, CNS infections, intra-abdominal infections including peritonitis, gynecological infections including pelvic inflammatory disease, endometritis and pelvic cellulites caused by susceptible strains of specific microorganisms; peri-operative prophylaxis.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Cefaclor Tablets",
+    id: "CAS NO:56-12-2",
+    d: [
+      "Cefaclor tablet is available in 250 mg, 500 mg compositions. It is a second-generation cephalosporin antibiotic used to treat certain infections caused by bacteria such as pneumonia and ear, lung, skin, throat, and urinary tract infections. Cefaclor should be used only to treat or prevent infections that are proven or strongly suspected to be caused by bacteria. Cefaclor is active against many bacteria, including both Gram-negative and Gram-positive organisms.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Cefdinir Tablets",
+    id: "CAS NO:91832-40-5",
+    d: [
+      "Cefdinir tablets is a semi-synthetic oral antibiotic in the cephalosporin family of antibiotics effective to treat infection caused by various Gram +ve and gram –ve bacteria, such as: Pneumonia , Bronchitis, Sinus infections, Tonsillitis, Ear infections, Skin infections. Cefdinir tablets is available in 300 mg.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Cefepime injection",
+    id: "CAS NO: 88040-23-7",
+    d: [
+      "Cefepime is usually reserved to treat severe nosocomial pneumonia, infections caused by multi-resistant microorganisms and empirical treatment of febrile neutropenia.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Cefixime Tablet ",
+    id: "CAS NO:79350-37-1",
+    d: [
+      "Cefixime tablet is available in 100mg and 200mg compositions. It is a oral third generation cephalosporin antibiotics used to treat a wide variety of bacterial infections. ",
+      "Our Cefixime antibiotic tablets are ideal for treatment of bacterial infections of the chest, ears, urinary tract, and throat and for uncomplicated gonorrhea.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Ceftazidime Injection",
+    id: "CAS NO: 72558-82-8",
+    d: [
+      "Ceftazidime is a third-generation semi synthetic, broad-spectrum, beta-lactum antibiotic for parenteral administration. Ceftazidime is indicated for the treatment of the following infections when known or likely to be due to one or more susceptible microorganisms and when parenteral therapy is required:",
+      "- Respiratory tract infections, including lower respiratory tract infections in patients with cystic fibrosis",
+      "- Urinary tract infections; Ceftazidime may also be used for peri-operative prophylaxis during trans-urethral prostatectomy",
+      "- Skin and soft tissue infections",
+      "- Biliary tract infections",
+      "- Intra-abdominal infections",
+      "- Bone and joint infections",
+      "- Infections associated peritoneal dialysis and with continuous ambulatory peritoneal dialysis (CAPD)",
+      "- Meningitis due to aerobic gram-negative organisms",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Ceftriaxone Injection",
+    id: "CAS NO: 73384-59-5",
+    d: [
+      "Ceftriaxone sodium injection is available in 250mg, 500mg,1gm strengths and is used to treat certain infections caused by Bacteria such as gonorrhea, pelvic inflammatory disease, meningitis, and infections of the lungs, ears, skin, urinary tract, blood, bones, joints, and abdomen.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Cefoperazone Injection",
+    id: "CAS NO:74578-69-1",
+    d: [
+      "Cefoperazone belongs to the class of third-generation cephalosporin’s. Cefoperazone is used in systemic treatment of infections, including those of the skin, bone, stomach, respiratory tract, sinuses, blood, and urinary tract.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Meropenem Injection ",
+    id: "CAS NO:96036-03-2",
+    d: [
+      "Meropenem injections (500mg, 1gm) are used for eliminating bacteria that cause several infections such as skin, bone, pneumonia and urinary tract, meningitis and stomach infections. This medication is known as a carbapenem-type antibiotic. It works by stopping the growth of bacteria.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Erythromycin Tablet",
+    id: "CAS NO: 114-07-8",
+    d: [
+      "Erythromycin is in a group of drugs called macrolide antibiotics. Macrolide antibiotics slow the growth of, or sometimes kill, sensitive bacteria by reducing the production of important proteins needed by the bacteria to survive.",
+      "Erythromycin is used to treat many different types of infections caused by bacteria.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Ciprofloxacin tablet",
+    id: "CAS NO:86393-32-0/85721-33-1",
+    d: [
+      "Ciprofloxacin film-coated tablets are available in 250 mg, 500 mg and 750 mg strengths. It is used to treat or prevent infections that are proven or strongly suspected to be caused by bacteria.",
+      "Ciprofloxacin is used to treat a number of infections including: infection of bones and joints, endocarditis, gastroenteritis, malignant otitis externa respiratory tract infections, urinary tract infections, anthrax etc. Ciprofloxacin is a chemotherapeutic antibiotic of the fluoro-quinolone.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Gatifloxacin tablet ",
+    id: "CAS NO:180200-66-2",
+    d: [
+      "Gatifloxin is an antibiotic of the fourth-generation fluoroquinolone family, inhibits the bacterial enzymes DNA gyrase and topoisomerase IV",
+      "Our antibiotic medicines, levofloxacin tablets are widely used for treating infections",
+      "These are available in 250mg, 500mg compositions, these are synthetic antibacterial agent of the flouroquinolone class and in the s(-) enantiomer of the racemic drug substance ofloxacin.",
+    ],
+    ul: {
+      h: "Our tablets are effective against :",
+      list: [
+        "pneumonia",
+        "chronic bronchitis",
+        "chronic bronchitis",
+        "sinus",
+        "urinary tract",
+        "kidney",
+        "skin infections",
+      ],
+    },
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Gentamycin injection",
+    id: "CAS NO:1405-41-0",
+    d: [
+      "Gentamicin injection is an aminoglycoside antibiotic, used to treat many types of bacterial infections, particularly those caused by Gram-negative bacteria. Gentamicin is a bactericidal antibiotic that works by binding the 30S subunit of the bacterial ribosome, interrupting protein synthesis.",
+      "Gentamicin is heat-stable antibiotics that remain active even after autoclaving, which makes it useful in the preparation of some microbiological growth media. It is used during orthopedic surgery when high temperatures are required for the setting of cements (e.g. hip replacements).It is available in 80 mg/2ml strength.",
+    ],
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Levofloxacin Tablet ",
+    id: "CAS NO:100986-85-4",
+    d: [
+      "Our antibiotic medicines, levofloxacin tablets are widely used for treating infections caused by certain bacteria and can be administered intravenously or orally. ",
+      "Levofloxacin is a quinolone antibiotic and works by killing sensitive bacteria. These are available in 250 mg, 500 mg compositions, these are synthetic antibacterial agent of the flouro quinolone class and in the s (-) enantiomer of the racemic drug substance ofloxacin. ",
+    ],
+    ul: {
+      h: "Our tablets are effective against :",
+      list: [
+        "pneumonia",
+        "chronic bronchitis ",
+        "sinus ",
+        "urinary tract ",
+        "kidney ",
+        "skin infections",
+      ],
+    },
+  },
+  {
+    c: "Antibiotic Medicines",
+    n: "Ofloxacin Tablet",
+    id: "CAS NO: 82419-36-1",
+    d: [
+      "Ofloxacin tablets (200mg, 400mg) helps fight against bacteria in the human body. These are effective in treating certain diseases that comprise : Bacterial infections including pneumonia, bronchitis, venereal disease (VD), Prostate, skin, and urinary tract infections, peritonitis and gonorrhea.",
+    ],
+  },
+  {
+    c: "Anticancer Medicines",
+    n: "Anastrazole Tablets",
+    id: "CAS NO: 82419-36-1",
+    d: [
+      "Ofloxacin tablets (200mg, 400mg) helps fight against bacteria in the human body. These are effective in treating certain diseases that comprise : Bacterial infections including pneumonia, bronchitis, venereal disease (VD), Prostate, skin, and urinary tract infections, peritonitis and gonorrhea.",
+    ],
+  },
+];
+
+
+export const productHeadings = [
+    "Antacids",
+    "Anthelmintics",
+    "Antibacterial Drugs",
+    "Antibiotic Medicines",
+    "Anticancer Medicines",
+    "Antidiabetic Drugs",
+    "Antimalarial Drugs",
+    "Antiobesity Drugs",
+    "Cardiovascular Drugs",
+    "Endocrine Disorder Drugs",
+    "Gynaecological Drugs",
+    "Mens Health",
+    "Steroids",
+    "Thyroid Drugs",
+    "Veterinary Drugs",
+    "Antiviral Drugs",
+    "Analgesics",
+    "Others",
+    "Herbal & Ayurvedic Products",
+]
