d3ef30a79779e31819489d856ec4481c187030a0
Ashutoshkumar75 <ashutoshkumar75mgr@gmail.com>
2021-06-13 16:58:02 +0530

    404 page added

--- components/404/NotFound.js
+++ components/404/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import {img} from './404.png'
 
 const NotFound = () => {
     return (
-        <div className="text-center">
-            <img className='w-full object-cover' src={img} alt="" />
+        <div className="text-center ">
+            <img className='w-1/2 md:w-1/2 lg:w-1/2 text-center mx-auto mt-16' src="/assets/404.png" alt="" />
+            <h5 className="text-gray-900 font-extrabold">Ooops!!</h5>
+            <p className="text-gray-400 font-bold my-2">THAT PAGE DOESN'T EXISTS OR IS UNAVAILABLE.</p>
+            <button className="bg-brand-200 text-white font-bold  p-3 my-8 focus:outline-none  "> <a href="/">Go Back to Home</a></button>
         </div>
     )
 }
