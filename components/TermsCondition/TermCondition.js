8cb4cdaa555aaf9f1779d51e40d5100ed7a60dcc
JUSTIN <justinjose320@gmail.com>
2021-06-19 20:45:56 +0530

    added new terms page

--- components/TermsCondition/TermCondition.js
+++ components/TermsCondition/TermCondition.js
@@ -2,115 +2,132 @@ import React from "react";
 
 const TermCondition = () => {
   return (
-    <div className="bg-gray-50">
-      <div className="border max-w-7xl mx-auto py-10 px-4">
-        <h5 className="text-3xl sm:text-4xl font-bold my-4">
-          TERMS & CONDITIONS
-        </h5>
-        <div className="p-4 md:p-10 mb-8 rounded-lg shadow-lg bg-white">
-          <h4 className="text-xl md:text-3xl font-bold mb-1 mt-0 text-left">
-            General
-          </h4>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-        </div>
-        <div className="p-4 md:p-10 mb-8 rounded-lg shadow-lg bg-white">
-          <h4 className="text-xl md:text-3xl font-bold mb-1 mt-0 text-left">
-            General
-          </h4>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-        </div>
-        <div className="p-4 md:p-10 mb-8 rounded-lg shadow-lg bg-white">
-          <h4 className="text-xl md:text-3xl font-bold mb-1 mt-0 text-left">
-            General
-          </h4>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-        </div>
-        <div className="p-4 md:p-10 mb-8 rounded-lg shadow-lg bg-white">
-          <h4 className="text-xl md:text-3xl font-bold mb-1 mt-0 text-left">
-            General
-          </h4>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-          <p className="mt-2 mb-6 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
-        </div>
-        <div className="p-4 md:p-10 mb-8 rounded-lg shadow-lg bg-white">
-          <h4 className="text-xl md:text-3xl font-bold mb-1 mt-0 text-left">
-            General
-          </h4>
-          <p className="my-2 text-base md:text-xl text-gray-500 ">
-            By using any Digital Work (Item) from TailWind UI Kit (AlphaSquad,
-            Inc.), you agree that this End User License Agreement (EULA) is a
-            legally binding and valid contract and agree to be bound by it. You
-            agree to abide by the intellectual property laws and all of the
-            terms and conditions of this Agreement. If you do not agree to be
-            bound by this agreement, remove any Item from your computer now.
-          </p>
+    <div>
+      <div className="max-w-5xl mx-auto p-2 md:p-4 pb-10">
+        <div className="bg-white rounded-lg shadow-lg">
+          <div
+            style={{
+              backgroundImage: "linear-gradient(310deg ,#48BCD1,#004d5e)",
+            }}
+            className="rounded-t-lg text-white py-12 px-4 md:px-8"
+          >
+            <h1 className="text-3xl font-bold">Terms & Conditions</h1>
+            <div className="flex">
+              <p className="font-medium text-gray-200">
+                Effective date:
+                <span>10th of May, 2021</span>{" "}
+              </p>
+            </div>
+          </div>
+          <div className="px-4 md:px-8 py-4">
+            {/* Page content */}
+            <div>
+              <h2 className="text-3xl font-bold my-4">Terms & Conditions</h2>
+              <p>
+                Welcome to StartupColab.in. StartupColab.in is owned and managed
+                by BYTEGENICS SOLUTIONS PRIVATE LIMITED. (herein after referred
+                to as StartupColab). If you continue to browse and use this
+                website you are agreeing to comply with and be bound by the
+                following terms and conditions of use, which together with our
+                privacy policy govern
+                StartupColab.com/StartupColabStartupColab's relationship with
+                you in relation to this website.
+              </p>
+              <p>
+                The term 'StartupColabStartupColab' or 'us' or 'we' refers to
+                the owner of the website whose registered office is A-201, 2nd
+                Floor, Galore Tech Lohia-Jain Bldg., Chandni Chowk, Road Opp. To
+                HP petrol pump, Bh. Maratha Mandir Hall, Bavdhan (Khurd), Pune -
+                411021. Our company registration is U74999MH2020PTC344992,
+                India. The term 'you' refers to the user or viewer of our
+                website.
+              </p>
+            </div>
+
+            <div className="py-1">
+              <h2 className="text-3xl font-bold my-4">Terms & Conditions</h2>
+
+              <ul className="list-disc ml-4 md:ml-10 my-4">
+                <li className="my-1">
+                  The content of the pages of this website is for your general
+                  information and use only. It is subject to change without
+                  notice.
+                </li>
+                <li className="my-1">
+                  {" "}
+                  Neither we nor any third parties provide any warranty or
+                  guarantee as to the accuracy, timeliness, performance,
+                  completeness or suitability of the information and materials
+                  found or offered on this website for any particular purpose.
+                  You acknowledge that such information and materials may
+                  contain inaccuracies or errors and we expressly exclude
+                  liability for any such inaccuracies or errors to the fullest
+                  extent permitted by law.
+                </li>
+                <li className="my-1">
+                  Your use of any information or materials on this website is
+                  entirely at your own risk, for which we shall not be liable.
+                  It shall be your own responsibility to ensure that any
+                  products, services or information available through this
+                  website meet your specific requirements.
+                </li>
+                <li className="my-1">
+                  This website contains material which is owned by or licensed
+                  to us. This material includes, but is not limited to, the
+                  design, layout, look, appearance and graphics. Reproduction is
+                  prohibited other than in accordance with the copyright notice,
+                  which forms part of these terms and conditions. All trademarks
+                  reproduced in this website which are not the property of, or
+                  licensed toStartupColab are acknowledged on the website.
+                </li>
+                <li className="my-1">
+                  Unauthorised use of this website may give rise to a claim for
+                  damages and/or be a criminal offence. From time to time this
+                  website may also include links to other websites. These links
+                  are provided for your convenience to provide further
+                  information. They do not signify that we endorse the
+                  website(s). We have no responsibility for the content of the
+                  linked website(s).
+                </li>
+                <li className="my-1">
+                  You may not create a link to this website from another website
+                  or document withoutStartupColab's prior written consent. Your
+                  use of this website and any dispute arising out of such use of
+                  the website is subject to the laws of India or regulatory
+                  authority within the country of India.
+                </li>
+              </ul>
+            </div>
+            <div className="py-1">
+              <h2 className="text-3xl font-bold my-4">Cancellation Policy</h2>
+              <p>
+                StartupColab believes in helping its customers as far as
+                possible, and has therefore a liberal cancellation policy. Under
+                this policy:
+              </p>
+              <ul className="list-disc ml-4 md:ml-10 my-4">
+                <li className="my-1">
+                  Cancellations will be considered only if the request is made
+                  within 24 hours of placing an order. However, the cancellation
+                  request will not be entertained if the orders have been
+                  communicated to the operational team and Operations team has
+                  initiated the process of assignment. Cancellation and refund
+                  of fees or charges will be strictly governed by the Company
+                  Refund Policy of the company informed and declared from time
+                  to time.
+                </li>
+                <li className="my-1">
+                  There is no cancellation of orders placed under the Same day
+                  delivery category. No cancellations are entertained for those
+                  products/services that theStartupColab marketing team has
+                  obtained on special discounts and offers on occasions like New
+                  Year, Pongal, Diwali, Independence Day, Foundation Day etc.
+                  These are limited occasion offers and therefore cancellations
+                  are not possible.
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
