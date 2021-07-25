8e571558f377a52ae6a741c8c79a00776e5ad109
JUSTIN <justinjose320@gmail.com>
2021-07-25 21:00:52 +0530

    terms

--- components/TermsCondition/TermCondition.js
+++ components/TermsCondition/TermCondition.js
@@ -15,31 +15,226 @@ const TermCondition = () => {
           </div>
           <div className="px-4 md:px-8 py-4 text-sm md:text-base">
             {/* Page content */}
-            <div className='text-justify'>
+            <div className="text-justify">
               {/* <h2 className="text-3xl font-bold my-4">Terms & Conditions</h2> */}
               <p className="mt-2">
-                We reserve the right to change these Terms of Service at any
-                time without notice in its discretion and to notify users of any
-                such changes solely by changing this Terms of Service. Your
-                continued use of the Website after the posting of any amended
-                Terms of Service shall constitute your agreement to be bound by
-                any such changes. Your use of this Website prior to the time
-                this Terms of Service was posted will be governed according to
-                the Terms of Service that applied at the time of your use.
+                You should carefully read these Terms and Conditions before
+                using our Website. By using this Site, you agree to be bound by
+                these Terms and Conditions. This is a legally binding agreement.
+              </p>
+              <ul className="my-4 list-decimal ml-4">
+                <li className="my-1">
+                  We agree to provide you access to our Site in accordance with
+                  the Terms and Conditions.
+                </li>
+                <li className="my-1">
+                  You agree to use our Site in a manner consistent with any and
+                  all applicable rules and regulations.
+                </li>
+                <li className="my-1">
+                  You accept that our Site is provided on an "AS IS, AS
+                  AVAILABLE" basis.
+                </li>
+                <li className="my-1">
+                  By placing an order through Our Site, You warrant that: You
+                  are at least 18 years of age.
+                </li>
+                <li className="my-1">
+                  ALL ARTICLES AND MATERIAL DISPLAYED BY US ON OUR SITE ARE FOR
+                  INFORMATION ONLY AND ARE NO SUBSTITUTE FOR SPECIFIC ADVICE.
+                </li>
+              </ul>
+              <p className="mt-2">
+                Kosher Pharmaceuticals collect from prospective or current
+                business contacts on our database or that individuals provide to
+                us through other means, how we may use that information, to whom
+                and under what circumstances we may disclose it, and how we
+                protect it; rights remain solely to us . By using this Site or
+                voluntarily providing personal information to us, you consent to
+                the Terms and Conditions. If you do not agree to the policy,
+                please do not use the Site.
+              </p>
+            </div>
+            <div className="text-justify">
+              <h2 className="text-base md:text-xl font-bold my-4">
+                Collection and Use of Personal Information for Business Purposes
+              </h2>
+              <p className="mt-2">
+                Kosher Pharmaceuticals collects, uses, and discloses personal
+                information of its business contacts for business purposes only
+                and consistent with applicable laws. Where Kosher
+                Pharmaceuticals discloses personal information to third parties,
+                it does so for the same business purposes described below and,
+                where appropriate, requires that such parties maintain its
+                confidentiality and maintain appropriate systems and processes
+                to ensure its security and protection.
+              </p>
+            </div>
+            <div className="text-justify">
+              <h2 className="text-base md:text-xl font-bold my-4">
+                Children’s Privacy
+              </h2>
+              <p className="mt-2 underline">Under Age 13</p>
+              <p className="mt-2">
+                We understand the need to protect children's privacy online (we
+                define “children” as minors younger than 13). We do not
+                knowingly collect or use any personal information from children
+                without verifiable parental consent. We do not knowingly allow
+                children to communicate with us, or to use any of our online
+                services. If it comes to our attention that we have unknowingly
+                collected information from a child under the age of 13, we will
+                take all reasonable measures to delete it as soon as possible
+                and will not use such information for any purpose (except where
+                necessary to protect the safety of the child or others as
+                required or allowed by law). If you become aware of any
+                personally identifiable information we have collected from
+                children under 13, please call us at +91 9769768968 or email us
+                at{" "}
+                <a
+                  href="mailto: info@kosherpharmaceuticals.com"
+                  target="_blank"
+                  className="text-brand-100 hover:text-brand-200"
+                >
+                  info@kosherpharmaceuticals.com
+                </a>
+                .
+              </p>
+              <p className="mt-2 underline">Under Age 18</p>
+              <p className="mt-2">
+                Minors under 18 years of age may have the personal information
+                that they provide to us deleted by sending an email to{" "}
+                <a
+                  href="mailto: info@kosherpharmaceuticals.com"
+                  target="_blank"
+                  className="text-brand-100 hover:text-brand-200"
+                >
+                  info@kosherpharmaceuticals.com
+                </a>{" "}
+                requesting deletion. Please note that, while we make reasonable
+                efforts to comply with such requests, deletion of your personal
+                information does ensure complete and comprehensive removal of
+                that data from all systems.
+              </p>
+            </div>
+            <div className="text-justify">
+              <h2 className="text-base md:text-xl font-bold my-4">
+                Consent and Use
+              </h2>
+              <p className="mt-2">
+                The submission of personal or business data (including e-mail
+                addresses, names and addresses) is done on an explicitly
+                voluntary basis. In submitting such data you indicate your
+                consent that such data may be collected, stored, processed or
+                used by and/or disclosed to Kosher Pharmaceuticals for the
+                purpose of making contact with you, responding to your requests
+                for information, or for other purposes indicated by you, as well
+                as for the protection of our own legitimate business interests.
+                Kosher Pharmaceuticals may also disclose personal or business
+                data as may be required by law or a court order, subpoena or
+                other similar legal instrument. Your consent to our use of your
+                personal data may be withdrawn by you at any time by contacting
+                Kosher Pharmaceuticals at{" "}
+                <a
+                  href="mailto: info@kosherpharmaceuticals.com"
+                  target="_blank"
+                  className="text-brand-100 hover:text-brand-200"
+                >
+                  info@kosherpharmaceuticals.com
+                </a>
+                .
+              </p>
+            </div>
+            <div className="text-justify">
+              <h2 className="text-xl md:text-3xl font-bold my-4">
+                TERMS OF USE
+              </h2>
+              <p className="mt-2">
+                Kosher Pharmaceuticals maintains this site for your personal
+                information, education, and communication. Please feel free to
+                browse the Site. You may download materials displayed on the
+                Site for non-commercial, personal use only, provided you also
+                retain all copyright and other proprietary notices contained in
+                the materials or otherwise on this Site. You may not, however,
+                distribute, modify, transmit, reuse, report, or use the contents
+                of the Site for public or commercial purposes, including the
+                text, images, audio or video, without advance written permission
+                from Kosher Pharmaceuticals. Your access and use of the Site is
+                also subject to the following Terms and Conditions (T&C) and all
+                applicable laws. By accessing and browsing the Site, you accept,
+                without limitation or qualification, the Terms and Conditions.
               </p>
               <p className="mt-2">
-                We may modify, suspend, discontinue or restrict the use of any
-                portion, including the availability of any portion of the
-                Content at any time, without notice or liability. We may deny
-                access to any person or user at any time for any reason. In
-                addition, we may at any time transfer rights and obligations
-                under this Agreement to any affiliate, subsidiary or business
-                unit, or any of their affiliated companies or divisions, or any
-                entity that acquires us or any of their assets.
+                The Site is intended to provide general information about Kosher
+                Pharmaceuticals, its products and services.
               </p>
               <p className="mt-2">
-                We hereby reserve the right to block usage of the Website if any
-                breach of the Terms of Service is caused by a User.
+                While Kosher Pharmaceuticals uses reasonable efforts to include
+                accurate and up-to-date information on the Site, the Site is not
+                meant to provide specialist advice concerning the products or
+                services of Kosher Pharmaceuticals. Kosher Pharmaceuticals makes
+                no warranties or representations as to the Site’s accuracy.
+                Kosher Pharmaceuticals disclaims all liability and
+                responsibility for any errors or omissions in the content on the
+                Site.
+              </p>
+              <p className="mt-2">
+                Kosher Pharmaceuticals also assumes no responsibility, and shall
+                not be liable for any damages to, or viruses that may infect,
+                your computer equipment or other property on account of your
+                access to, use of, or browsing in the Site or your downloading
+                of any materials, data, text or images or your viewing of any
+                video or audio from the Site.
+              </p>
+              <p className="mt-2">
+                Any communication or material you transmit to the Site by
+                electronic mail or otherwise through electronic forms included
+                on this Site, including any non-personal data, questions,
+                comments, suggestions, requests or the like is, and will be
+                treated as, non-confidential and non-proprietary. Anything you
+                transmit or post may be used without restriction by Kosher
+                Pharmaceuticals for any purpose, including but not limited to
+                reproduction, disclosure, transmission, publication, broadcast,
+                and posting. Furthermore, Kosher Pharmaceuticals is free to use
+                any ideas, concepts, know-how, or techniques contained in any
+                communication you send to the Site for any purpose whatsoever,
+                including but not limited to developing, manufacturing, and
+                marketing products using such information.
+              </p>
+              <p className="mt-2">
+                By using this Site, you agree not to disrupt or intercept our
+                electronic information posted on this Site or on any of our
+                servers. You also agree not to attempt to circumvent any
+                security features of the Site, and to abide by all applicable
+                local, state, federal and international laws, rules and
+                regulations.
+              </p>
+            </div>
+            <div className="text-justify">
+              <h2 className="text-base md:text-xl font-bold my-4">
+                Changes in this Privacy Policy
+              </h2>
+              <p className="mt-2">
+                Kosher Pharmaceuticals reserves the right to modify this Privacy
+                Policy at any time, so please review it frequently. Your use of
+                this Site after changes to this Privacy Policy have been made
+                constitutes acceptance of those changes going forward.
+              </p>
+            </div>
+            <div className="text-justify">
+              <h2 className="text-base md:text-xl font-bold my-4">
+                Contact Us
+              </h2>
+              <p className="mt-2">
+                For questions about this privacy policy, or to request this
+                privacy policy in another format, please contact us at{" "}
+                <a
+                  href="mailto: info@kosherpharmaceuticals.com"
+                  target="_blank"
+                  className="text-brand-100 hover:text-brand-200"
+                >
+                  info@kosherpharmaceuticals.com
+                </a>
+                .
               </p>
             </div>
           </div>
